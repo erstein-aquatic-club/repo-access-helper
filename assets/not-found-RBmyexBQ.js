2ca3942505aea664e3194530ebbf6385f65e48b7
erstein-aquatic-club <erstein-aquatic-club@users.noreply.github.com>
2026-02-12 21:48:51 +0000

    deploy: e000dd06c3c1eae7fa3fd8bf858dee712b0b8e28

---
+++ assets/not-found-RBmyexBQ.js
@@ -0,0 +1,6 @@
+import{j as e}from"./vendor-ui-C9ymspZo.js";import{h as t,C as r,f as s}from"./index-w265yi4m.js";import"./vendor-react-BzrpNAyj.js";import"./vendor-query-kM2vrB_e.js";import"./vendor-charts-DSuxWnoS.js";import"./vendor-supabase-B2MyMyC0.js";/**
+ * @license lucide-react v0.545.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const a=[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["line",{x1:"12",x2:"12",y1:"8",y2:"12",key:"1pkeuh"}],["line",{x1:"12",x2:"12.01",y1:"16",y2:"16",key:"4dfq90"}]],o=t("circle-alert",a);function x(){return e.jsx("div",{className:"min-h-screen w-full flex items-center justify-center bg-background",children:e.jsx(r,{className:"w-full max-w-md mx-4",children:e.jsxs(s,{className:"pt-6",children:[e.jsxs("div",{className:"flex mb-4 gap-2",children:[e.jsx(o,{className:"h-8 w-8 text-destructive","aria-hidden":"true"}),e.jsx("h1",{className:"text-2xl font-bold text-foreground",children:"404 Page Not Found"})]}),e.jsx("p",{className:"mt-4 text-sm text-muted-foreground",children:"Did you forget to add the page to the router?"})]})})})}export{x as default};
